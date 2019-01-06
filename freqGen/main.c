9b4273a46a866ef4fcec2a7c5f376ace96b51205
Dominique Stender <dominique.stender@gmail.com>
2019-01-06 23:16:53 +0100

    fixed tabs in c file

--- freqGen/main.c
+++ freqGen/main.c
@@ -16,19 +16,19 @@ const float BASE_NOTE = 32.66; // C1 is 32.70 Hertz
 const float BASE_INCR = 1.955; // set this to 2.0 as default
 
 int main() { 
-	printf("const uint16_t freqTable[] PROGMEM = {\n");
-	
+  printf("const uint16_t freqTable[] PROGMEM = {\n");
+
   for (int c = 0; c < 1024; c++) {
     float f = 5 * c / 1024.0;
-    
+
     printf("%.0f, ", floor((65536 * BASE_NOTE * pow(BASE_INCR, f)) / 31250 + 0.5f)); 
-		
-		if (c % 10 == 0) {
-			printf("\n");
-		}
+
+    if (c % 10 == 0) {
+      printf("\n");
+    }
   }
- 
-	printf("\n};");
-	
+
+  printf("\n};");
+
   return 0;
 } 
